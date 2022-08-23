a08b2db7f9a7eb358acc0afa6e391d677d3be335
Yartseva Ksenia <kseniyasid@gmail.com>
2022-08-23 20:22:09 +0500

    Дописывает скрипт для меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,6 +1,8 @@
 let burger = document.querySelector('.navigation__toggle');
 let menu = document.querySelector('.navigation__list');
 
+menu.classList.remove('navigation__list--open')
+
 burger.onclick = function () {
   burger.classList.toggle('navigation__toggle--cross');
   menu.classList.toggle('navigation__list--open');
