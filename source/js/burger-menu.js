531da898c5f85858a03e28ca954f759a03daa206
Yartseva Ksenia <kseniyasid@gmail.com>
2022-08-25 10:53:53 +0500

    Дорабатывает открытие/закрытие меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,7 +1,7 @@
 let burger = document.querySelector('.navigation__toggle');
 let menu = document.querySelector('.navigation__list');
 
-menu.classList.remove('navigation__list--open')
+menu.classList.remove('navigation__list--nojs')
 
 burger.onclick = function () {
   burger.classList.toggle('navigation__toggle--cross');
