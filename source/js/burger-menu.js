30992a7bb623b69ea0cc7a47e637ef9392934d96
Yartseva Ksenia <kseniyasid@gmail.com>
2022-09-14 09:58:16 +0500

    Добавляет место под лого без js

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,7 +1,9 @@
+let navigation = document.querySelector('.navigation');
 let burger = document.querySelector('.navigation__toggle');
 let menu = document.querySelector('.navigation__list');
 let logo = document.querySelector('.main-header__logo');
 
+navigation.classList.remove('navigation--nojs');
 menu.classList.remove('navigation__list--nojs');
 burger.classList.add('navigation__toggle--withjs');
 logo.classList.add('main-header__logo--withjs');
